8a5ac77d7ee48a9dbbe32d94d9247d9ad9d7a329
Ross Nelson <ross.nelson@gmail.com>
2014-01-08 10:04:11 -0600

    Defaulted to thin over WEBrick

--- app.rb
+++ app.rb
@@ -4,19 +4,25 @@ require 'liquid'
 require 'sass'
 Bundler.require
 
+configure do
+  set :app_file, __FILE__
+  set :run, true
+  set :server, %w[thin mongrel webrick]
+end
+
 # CSS running through SCSS
 get '/css/*.css' do
-	content_type 'text/css', :charset => 'utf-8'
-	scss params[:splat].join.to_sym, :style => :compressed
+  content_type 'text/css', :charset => 'utf-8'
+  scss params[:splat].join.to_sym, :style => :compressed
 end
 
 # Plain old URL with a Liquid template
 get '/' do
-  	liquid :index, :locals => { :name => 'world' }
+  liquid :index, :locals => { :name => 'world' }
 end
 
 # ERB, and routes with parameters
 get '/hello/:name' do
-	resp = "Hello, #{params[:name]}!"
-	erb resp
+  resp = "Hello, #{params[:name]}!"
+  erb resp
 end
